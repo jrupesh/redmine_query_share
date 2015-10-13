623488bb88893ec2c65c4536cb5a8806e1a8f3a4
Rupesh J <j.rupesh@gmail.com>
2015-10-13 15:21:32 +0530

    This should fixes #2, to be checked in 2.6 Version.

--- init.rb
+++ init.rb
@@ -1,16 +1,16 @@
 Rails.configuration.to_prepare do
   require 'query_share/patches/issue_query_patch'
   require 'query_share/patches/issues_helper_patch'
-  require 'query_share/patches/queries_controller_patch'
   require 'query_share/patches/query_patch'
   require 'query_share/patches/user_patch'
+  require 'query_share/patches/queries_controller_patch' if Redmine::VERSION::MAJOR >= 3
 end
 
 Redmine::Plugin.register :redmine_query_share do
   name 'Redmine Query share'
   author 'Rupesh J'
   description 'Share your redmine queries among specific user across user roles.'
-  version '1.0.2'
+  version '1.0.3'
   author_url 'mailto:rupeshj@esi-group.com'
 
   settings :default => {  :query_share_enable => false },
