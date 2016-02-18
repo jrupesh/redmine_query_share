0e34ec27dc49307720c2505f5e895de553e0ad2d
Rupesh J <j.rupesh@gmail.com>
2016-02-18 13:09:47 +0530

    Clean up and set private if the user is not allowed with the group visibility

--- lib/query_share/patches/queries_controller_patch.rb
+++ lib/query_share/patches/queries_controller_patch.rb
@@ -21,13 +21,15 @@ module QueryShare
           share_visibility = params[:query] && params[:query][:visibility]
 
           if (User.current.allowed_to?(:manage_group_queries, @query.project) || User.current.admin?) &&
-           share_visibility.to_i == 3
-
-            logger.debug("Share visibility")
+           share_visibility.to_i == IssueQuery::VISIBILITY_GROUP
             @query.principal_ids = params[:query] && params[:query][:principal_ids]
             @query.principals_logins = params[:query] && params[:query][:principals_logins]
             @query.visibility = share_visibility
+          elsif @query.visibility.to_i == IssueQuery::VISIBILITY_GROUP
+            @query.visibility = IssueQuery::VISIBILITY_PRIVATE
           end
+
+          @query
         end
       end
     end
