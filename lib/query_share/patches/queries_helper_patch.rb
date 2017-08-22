7ca059d25a077f99e08c5ae4f670230a849a5d5d
Rupesh J <j.rupesh@gmail.com>
2017-08-23 00:33:40 +0530

    Redmine 3.4.2 Update

--- lib/query_share/patches/queries_helper_patch.rb
+++ lib/query_share/patches/queries_helper_patch.rb
@@ -9,4 +9,33 @@ module QueriesHelper
                       'custom_values.custom_field_id' => Setting.plugin_redmine_query_share["group_cf"] ).map{
                         |s| { label: s.name, value: s.lastname } }
   end
+
+  def query_links_with_share(title, queries)
+    return '' if queries.empty?
+    return query_links_without_share(title, queries) unless Setting.plugin_redmine_query_share['query_share_enable'] == "1"
+    # links to #index on issues/show
+    url_params = controller_name == 'issues' ? {:controller => 'issues', :action => 'index', :project_id => @project} : params
+
+    content_tag('h3', title) + "\n" +
+        content_tag('ul',
+                    queries.collect {|query|
+                      css = 'query'
+                      css << ' selected' if query == @query
+                      css << " visible#{query.visibility}"
+                      content_tag('li', link_to(query.name, url_params.merge(:query_id => query), :class => css,
+                                                :title => l(:field_author) + ": #{query.user.name}"))
+                    }.join("\n").html_safe,
+                    :class => 'queries'
+        ) + "\n"
+  end
+  alias_method_chain :query_links, :share
+
+  def sidebar_queries_with_share(klass, project)
+    return sidebar_queries_without_share(klass, project) unless Setting.plugin_redmine_query_share['query_share_enable'] == "1"
+    unless @sidebar_queries
+      @sidebar_queries = klass.visible.global_or_on_project(@project).sorted.joins(:user).to_a
+    end
+    @sidebar_queries
+  end
+  alias_method_chain :sidebar_queries, :share
 end
